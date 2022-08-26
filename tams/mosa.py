234924e5f8b2f69e5047888252251ca3b8410dad
zmoon <zmoon92@gmail.com>
2022-08-25 20:31:05 -0600

    wip: saving single file of identify results

--- tams/mosa.py
+++ tams/mosa.py
@@ -8,6 +8,8 @@ import xarray as xr
 BASE_DIR = Path("/glade/campaign/mmm/c3we/prein/SouthAmerica/MCS-Tracking")
 """Base location on NCAR GLADE.
 
+Note that this location is only accessibly on Casper, not Hera!
+
 ├── GPM
 │   ├── 2000
 │   ├── 2001
@@ -39,3 +41,37 @@ def load_wrf(files):
     ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180))
 
     return ds
+
+
+import warnings
+
+import geopandas as gpd
+
+import tams
+
+ds = tams.load_example_mpas().rename(tb="ctt", precip="pr").isel(time=1)
+
+# Pre-process one time (for MOSA one file)
+
+# Identify CEs
+ce, _ = tams.core._identify_one(ds.ctt, ctt_threshold=241, ctt_core_threshold=225)
+ce = (
+    ce[["geometry", "area_km2", "area219_km2"]]
+    .rename(columns={"area219_km2": "area_core_km2"})
+    .convert_dtypes()
+)
+
+# Get precip stats
+df = tams.data_in_contours(ds.pr, ce, agg=("mean", "max", "min", "count"), merge=True)
+
+# Save to file
+# Get `pyarrow` from conda-forge
+# GeoParquet spec v0.4.0 requires GeoPandas v0.11 (which no longer warns)
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", message=".*initial implementation of Parquet.*")
+    df.to_parquet("t.parquet")
+
+# Load and check
+df2 = gpd.read_parquet("t.parquet")
+
+assert df2.equals(df)
