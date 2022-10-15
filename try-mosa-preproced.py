6f377e473e7d81be1041ee9af46777c36d855321
zmoon <zmoon92@gmail.com>
2022-10-15 15:08:43 -0600

    Move sample data files

--- try-mosa-preproced.py
+++ try-mosa-preproced.py
@@ -7,14 +7,15 @@ import xarray as xr
 
 from tams.mosa import run_wrf_preproced
 
-files = sorted(Path("~/Downloads/mosa-pre-sample").expanduser().glob("tb_rainrate*.parquet"))
+base = Path("~/OneDrive/w/ERT-ARL/mosa").expanduser()
+files = sorted((base / "mosa-pre-sample").glob("tb_rainrate*.parquet"))
 
 # DataFrame output
 df = run_wrf_preproced(files, id_="df")
 
 # Dataset output
 ds_grid = (
-    xr.open_dataset("~/Downloads/mosa-pre-sample/tb_rainrate_2010-06-01_01%3A00.nc")
+    xr.open_dataset(base / "mosa-pre-sample/tb_rainrate_2010-06-01_01%3A00.nc")
     .rename_dims({"rlat": "y", "rlon": "x"})
     .squeeze()
 )
