d45484c1774370b63e4d5a0df20c9b0689540350
zmoon <15079414+zmoon@users.noreply.github.com>
2022-10-15 20:00:08 -0600

    Initial job for nc production

--- run-mosa2.py
+++ run-mosa2.py
@@ -53,21 +53,50 @@ assert not extra
 #
 
 
-def run_wy(wy: int, files: list[Path]):
+def run_wy(wy: int, files: list[Path], rt="df"):
+    # Grid?
+    if rt == "ds":
+        import xarray as xr
+
+        p_grid = (
+            f"/glade/campaign/mmm/c3we/prein/SouthAmerica/MCS-Tracking/WY{wy}/WRF/"
+            f"tb_rainrate_{wy - 1}-06-01_00:00.nc"
+        )
+        grid = xr.open_dataset(p_grid).rename_dims({"rlat": "y", "rlon": "x"}).squeeze()
+    else:
+        grid = None
+
     # Run
-    ce = run_wrf_preproced(files, id_=f"WY{wy}")
+    ret = run_wrf_preproced(files, id_=f"WY{wy}", rt=rt, grid=grid)
 
-    # Only CEs associated to MCS (as defined by MOSA)
-    mcs = ce[ce.is_mcs].reset_index(drop=True).drop(columns=["is_mcs", "not_is_mcs_reason"])
-    mcs
+    if rt == "df":
+        # Only CEs associated to MCS (as defined by MOSA)
+        ce = ret
+        mcs = ce[ce.is_mcs].reset_index(drop=True).drop(columns=["is_mcs", "not_is_mcs_reason"])
+        mcs
 
-    # Save df
-    mcs.to_csv(OUT_DIR / f"wrf_wy{wy}.csv.gz", index=False)
+        # Save df
+        mcs.to_csv(OUT_DIR / f"wrf_wy{wy}.csv.gz", index=False)
+
+    elif rt == "ds":
+        ds = ret
+
+        # Save ds
+        # <last_name>_WY<YYYY>_<DATA>_SAAG-MCS-mask-file.nc
+        # DATA can either be OBS or WRF
+        ds.to_netcdf(OUT_DIR / f"TAMS_WY{wy}_WRF_SAAG-MCS-mask-file.nc")
+
+    else:
+        raise ValueError(f"invalid `rt` {rt!r}")
 
 
 if __name__ == "__main__":
+    from functools import partial
+
     import joblib
 
+    fn = partial(run_wy, rt="ds")
+
     joblib.Parallel(n_jobs=-2, verbose=1)(
-        joblib.delayed(run_wy)(wy, files) for wy, files in wy_files.items()
+        joblib.delayed(fn)(wy, files) for wy, files in wy_files.items()
     )
