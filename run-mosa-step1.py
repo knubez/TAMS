126d8d2b9e44412bb469e47bc0cde573e496462f
zmoon <zmoon92@gmail.com>
2022-11-04 19:21:49 -0600

    Initial GPM preproc script

--- run-mosa-step1.py
+++ run-mosa-step1.py
@@ -11,30 +11,58 @@ NOTE: took 8.7 min for one WY using 19 procs
 from functools import partial
 from pathlib import Path
 
-from tams.mosa import BASE_DIR, preproc_wrf_file
+import pandas as pd
+
+from tams.mosa import BASE_DIR, preproc_gpm_file, preproc_wrf_file
 
 # Constants
 out_dir = Path("/glade/scratch/zmoon/mosa-pre")
 parallel = True
+do_wrf = True
+do_gpm = True
 wys = [2011, 2016, 2019]
 
 assert out_dir.is_dir()
-preproc = partial(preproc_wrf_file, out_dir=out_dir)
 
 if parallel:
     import joblib
 
-for wy in wys:
-    files = sorted((BASE_DIR / f"WY{wy}" / "WRF").glob("tb_rainrate_*.nc"))
-    print(files[0])
-    print("...")
-    print(files[-1], f"({len(files)} total)")
 
-    # Remember that the first file has no CTT!
-    files = files[1:]
-
+def proc(fn, files):
     if parallel:
-        joblib.Parallel(n_jobs=-2, verbose=10)(joblib.delayed(preproc)(fp) for fp in files)
+        joblib.Parallel(n_jobs=-2, verbose=10)(joblib.delayed(fn)(fp) for fp in files)
     else:
         for fp in files:
-            preproc(fp)
+            fn(fp)
+
+
+# WRF
+if do_wrf:
+    fn = partial(preproc_wrf_file, out_dir=out_dir)
+    for wy in wys:
+        files = sorted((BASE_DIR / f"WY{wy}" / "WRF").glob("tb_rainrate_*.nc"))
+        print(files[0])
+        print("...")
+        print(files[-1], f"({len(files)} total)")
+
+        # Remember that the first file has no CTT!
+        files = files[1:]
+
+        proc(fn, files)
+
+
+# GPM
+if do_gpm:
+    fn = partial(preproc_gpm_file, out_dir=out_dir)
+    for wy in wys:
+        # The WRF file sets start in June of previous year
+        ts = pd.date_range(f"{wy - 1}/06/01", "{wy}/06/01", freq="H")[:-1]
+        rfns = ts.strftime(r"%Y") + "/merg_" + ts.strftime(r"%Y%m%d%H") + "_4km-pixel.nc"
+        files = [BASE_DIR / "GPM" / rfn for rfn in rfns]
+        print(files[0])
+        print("...")
+        print(files[-1], f"({len(files)} total)")
+
+        # Don't need to drop first file
+
+        proc(fn, files)
