dfea016f60a0035b65e889dc47a0322a76c8b9d6
zmoon <15079414+zmoon@users.noreply.github.com>
2023-10-06 16:55:20 -0600

    notes

---
+++ examples/dyamond/data.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+base = Path("/glade/campaign/mmm/c3we/prein/Papers/2023_Zhe-MCSMIP")
+# Under here we have 'Summer' and 'Winter' dirs
+# and then model dirs with 'olr_pcp_instantaneous' subdirs
+# that have the nc files, one for each hour
+# e.g.
+# /glade/campaign/mmm/c3we/prein/Papers/2023_Zhe-MCSMIP/Winter/UM/olr_pcp_instantaneous/pr_rlut_um_winter_2020012007.nc
