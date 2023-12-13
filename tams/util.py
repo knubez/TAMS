bf552207d88753f89bc0f916d257d1335aac109c
zmoon <zmoon92@gmail.com>
2023-12-12 19:52:51 -0700

    Fix hours since in colorbar
    wasn't actually hours since, was just time steps

--- tams/util.py
+++ tams/util.py
@@ -44,6 +44,7 @@ def plot_tracked(
     """Plot CEs at a range of times (colors) with CE group ID (MCS ID) identified."""
 
     import matplotlib.pyplot as plt
+    import pandas as pd
     from matplotlib import patheffects
 
     valid_backgrounds = {"map", "countries", "none"}
@@ -96,12 +97,20 @@ def plot_tracked(
 
             ax.set(xlabel="lon [°E]", ylabel="lat [°N]")
 
-    nt = cs.time.unique().size
-    colors = plt.cm.GnBu(np.linspace(0.2, 0.85, nt))
+    t = pd.Series(sorted(cs.time.unique()))
+    tmin, tmax = t.iloc[0], t.iloc[-1]
+    dt = t.diff().min()
+
+    def get_color(t_):
+        if tmin == tmax:
+            return plt.cm.tab10.colors[0]
+        else:
+            x = (t_ - tmin) / (tmax - tmin) * 0.65 + 0.2
+            return plt.cm.GnBu(x)
 
     # Plot blobs at each time
-    for i, (_, g) in enumerate(cs.groupby("time")):
-        color = colors[i]
+    for t_, g in cs.groupby("time"):
+        color = get_color(t_)
         blob_kwargs.update(facecolor=color, edgecolor=color)
         text_kwargs.update(color=color)
 
@@ -131,10 +140,10 @@ def plot_tracked(
             cbar_kwargs = {}
         cbar_kwargs = {**cbar_kwargs_default, **cbar_kwargs}
 
-        m = mpl.cm.ScalarMappable(
-            cmap=mpl.colors.ListedColormap(colors),
-            norm=mpl.colors.Normalize(vmin=0, vmax=nt),
-        )
+        cmap = mpl.colors.ListedColormap([get_color(t_) for t_ in t])
+        hours = ((t - tmin).dt.total_seconds() / 3600).values
+        hours = np.append(hours, hours[-1] + dt.total_seconds() / 3600)
+        m = mpl.cm.ScalarMappable(cmap=cmap, norm=mpl.colors.BoundaryNorm(hours, cmap.N))
         plt.colorbar(m, ax=ax, **cbar_kwargs)
 
 
