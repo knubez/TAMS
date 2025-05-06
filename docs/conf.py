9261f23b0ea5255c3bb3fb312ab2c05c305bac57
zmoon <zmoon92@gmail.com>
2025-05-06 13:59:45 -0500

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ project = "tams"
 html_title = "TAMS"
 html_logo = "_static/TAMS-logo.png"
 author = "K. M. Núñez Ocasio and Z. Moon"
-copyright = "2022\u20132024, K. M. Núñez Ocasio and Z. Moon"
+copyright = "2022\u20132025, K. M. Núñez Ocasio and Z. Moon"
 
 extensions = [
     "sphinx.ext.autodoc",
