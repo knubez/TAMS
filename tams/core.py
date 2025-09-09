73131881b75859d064e30e3960371d72e8b3a9c8
zmoon <zmoon92@gmail.com>
2025-09-09 14:36:06 -0500

    Fix `nce`

--- tams/core.py
+++ tams/core.py
@@ -1055,7 +1055,7 @@ def run(
                 )
             )
 
-        d["nce"] = len(mcs_)  # codespell:ignore nce
+        d["nce"] = time_group.size()  # codespell:ignore nce
         d["area_km2"] = time_group.area_km2.sum()
         d["area219_km2"] = time_group.area219_km2.sum()
         # TODO: compare to re-computing area after (could be different if shift to dissolve)?
